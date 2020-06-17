758612dc60e9c017bf9f0df30a2cd214191a79b7
Teja <teja.chundru@skillsoft.com>
2020-06-17 21:49:45 +0530

    Updates

---
+++ precache-manifest.aa8e6db631db8f1d0f4f874d22d2032e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd878f63b1c7ee77b81e1777bd818bbe",
+    "url": "/covidandhra.com/index.html"
+  },
+  {
+    "revision": "e2ad034f3508127eb7b7",
+    "url": "/covidandhra.com/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7a39a4a3c84e08d56662",
+    "url": "/covidandhra.com/static/js/2.66e98e0b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covidandhra.com/static/js/2.66e98e0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2ad034f3508127eb7b7",
+    "url": "/covidandhra.com/static/js/main.4d6c6255.chunk.js"
+  },
+  {
+    "revision": "ac6d06b5f58db2b10056",
+    "url": "/covidandhra.com/static/js/runtime-main.46c5108f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covidandhra.com/static/media/logo.5d5d9eef.svg"
+  }
+]);
